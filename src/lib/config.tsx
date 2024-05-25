4f72056c1722d43d9b613930854b6c34fbd52ff3
Lexxi <phaelyxm@gmail.com>
2024-05-25 22:01:02 +0100

    blork

---
+++ src/lib/config.tsx
@@ -0,0 +1,10 @@
+export interface Config{
+    tempUnit: string,
+    windSpeedUnit: string,
+    pressureUnit: string,
+    colourScheme: string,
+    staticColour: string,    
+    selectedCountry: string , 
+    selectedCity: string,
+    todoistApiKey: string
+}
